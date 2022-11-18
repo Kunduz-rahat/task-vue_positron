79a6e648a6042a9ae97ce1b012463c8cbb729813
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-11-18 13:58:30 +0600

    add products

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+	<div v-for="product in products" v-bind:key="product.id">
+		<h1>{{product.title}}</h1>
+		<img :src="product.image"/>
+	</div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+	data(){
+		return{
+		products:[
+{  id:1, 
+	title:"BXC", 
+	image:'./../assets/BXC.png',
+	desk:"Вытяжное устройство для механической системы вентиляции", 
+	price:12644, 
+	vendorcode:"G2H1065", 
+	description:"12-72/168м³/ч/ гидрорегулируемый расходы/ от датчика присутствия"},
+{  id:2, 
+	title:"G2H", 
+	image:'./../assets/G2H.png',
+	desk:"Многофункциональное вытяжное устройство для естественной и гибридной вентиляции", 
+	price:12644, 
+	vendorcode:"G2H1065", 
+	description:"12-72/168м³/ч/ гидрорегулируемый расходы/ от датчика присутствия"},
+{  id:3, 
+	title:"GHN", 
+	image:'./../assets/GHN.png',
+	desk:"Вытяжное устройство с датчиком присутствия", 
+	price:12644, 
+	vendorcode:"G2H1065", 
+	description:"12-72/168м³/ч/ гидрорегулируемый расходы/ от датчика присутствия"},
+{  id:4, 
+	title:"TDA",
+	image:'./../assets/TDA.png', 
+	desk:"Вытяжное устройство с датчиком присутствия", 
+	price:12644, 
+	vendorcode:"G2H1065", 
+	description:"12-72/168м³/ч/ гидрорегулируемый расходы/ от датчика присутствия"}
+]
+		}
+		
+	},
+	methods:{
+		
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
