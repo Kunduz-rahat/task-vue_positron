856eb725ba1b57a8198b064a2fea1cbc7d4f02ea
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-11-19 23:03:16 +0600

    finished a product list

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -2,12 +2,16 @@
 	<div class="product__item">
 		
 		<img :src="product.src"  class="product__item_img"/>
-		<h2>{{product.title}}</h2>
-		<p>{{product.desk}}</p>
-		<p>6 848 ₽ – 56 584 ₽</p>
-		<p>77.60 € – 643.86 €</p>
+		<h2 class="product__title">{{product.title}}</h2>
+		<p class="product__subtitle">{{product.desk}}</p>
+		<div class="product_price">
+		
+		<p class="product__price_r">6 848 ₽ – 56 584 ₽</p>
+		<p class="product__price_e">77.60 € – 643.86 €</p>
 		<button class="button">Подробнее</button>
 	
+		</div>
+		
 	</div>
 		
 </template>
@@ -39,6 +43,7 @@ line-height: 145%;
 text-align: center;
 color: #FFFFFF;
 border: none;
+width: 100%;
 }
 .product__item{
 	background: #F6F8FA;
@@ -50,4 +55,43 @@ padding: 25px;
 	max-width: 245px;
 	min-width: 245px;
 }
+.product__title{
+	font-family: 'Lato';
+font-style: normal;
+font-weight: 700;
+font-size: 22px;
+line-height: 120.52%;
+color: #1F2432;
+}
+.product__subtitle{
+	font-family: 'Lato';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 145%;
+color: #1F2432;
+width: 255px;
+}
+.product__price{
+	margin-top: 40px;
+}
+.product__price_r{
+	font-family: 'Roboto';
+font-style: normal;
+font-weight: 500;
+font-size: 22px;
+line-height: 130%;
+letter-spacing: 0.005em;
+color: #1F2432;
+}
+
+.product__price_e{
+	font-family: 'Lato';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 145%;
+color: #797B86;
+}
+
 </style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,18 @@
 <template>
 	<div >
-		<h1>Просмотренные товары</h1>
+		<div class="product__header">
+			<h1 class="subtitle">Просмотренные товары</h1>
+			<div class="page">
+				<div class="page_nav">
+					<img src='./../assets/Right.png'/>
+				</div>
+				<p class="page__number"><span class="page__active">1</span> / 6</p>
+				<div class="page_nav">
+					<img src='./../assets/Left.png'/>
+				</div>
+			</div>
+		</div>
+		
 	<div class="product__list">
 		<product-item v-for="product in products" 
 		:product="product" 
@@ -29,9 +41,52 @@ import ProductItem from './ProductItem.vue';
 
 <style  scoped>
 .product__list{
-	
+margin-top: 60px;
 	display: grid;
-	grid-template-columns: 1fr 1fr 1fr 1fr;
-	grid-column: 20px;
+	grid-template-columns: repeat(4, 1fr);
+	grid-column-gap: 20px;
+}
+.subtitle{
+	font-family: 'Lato';
+font-style: normal;
+font-weight: 600;
+font-size: 30px;
+line-height: 120.52%;
+color: #1F2432;
+}
+.page{
+	display: flex;
+	align-items: center;
+	
+}
+.page_nav{
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
+	background: #90A2B5;
+transform: rotate(-180deg);
+display: flex;
+align-items: center;
+justify-content: center;
+}
+.page__active{
+	font-weight: 500;
+font-size: 24px;
+line-height: 150%;
+color: #212121;
+}
+.page__number{
+	font-family: 'Lato';
+font-style: normal;
+font-size: 16px;
+line-height: 145%;
+color: #797B86;
+margin:  0 15px ;
+
+}
+.product__header{
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 </style>
